b3116bb4240dc21666de5abb58ca1adfb1813fde
Siratul Islam <siratulislam@Siratuls-Air.Dlink>
2024-07-14 11:16:24 +0600

    RoadAhead map

--- app/(tabs)/map.jsx
+++ app/(tabs)/map.jsx
@@ -1,12 +1,72 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Map = () => {
+  const [region, setRegion] = useState({
+    latitude: 23.8103,
+    longitude: 90.4125,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.warn("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      let initialPosition = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      };
+      setRegion(initialPosition);
+    };
+
+    requestLocationPermission();
+  }, []);
+
   return (
-    <View>
-      <Text>Map</Text>
-    </View>
+    <SafeAreaView className="h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View style={styles.container}>
+          <MapView
+            region={region}
+            style={styles.map}
+            showsMyLocationButton={true}
+            showsUserLocation={true}
+            onUserLocationChange={(event) => {
+              console.log(
+                "User's current location:",
+                event.nativeEvent.coordinate
+              );
+            }}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default Map;
